1e14b722389b936b317630b1b271c7f40ebfee6c
SUNSUNABCSUN <2697535532@qq.com>
2015-02-10 04:27:12 +0000

    Basic detection version
    low success

--- main/main.py
+++ main/main.py
@@ -41,7 +41,7 @@ c = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
 # compute the rotated bounding box of the largest contour
 rect = cv2.minAreaRect(c)
 box = np.int0(cv2.cv.BoxPoints(rect))
-print box
+# print box
 
 # draw a bounding box arounded the detected barcode and display the
 # image
@@ -68,66 +68,102 @@ cv2.namedWindow("Final_2")
 cv2.imshow("Final_2",dst)
 
 #gain the shap of the cutted pic
-
-m, n = dst.shape[:2]
-bar_y = np.zeros((500,255))
-bar_num = np.zeros((500,255))
-bar_int = np.zeros(500,dtype=int)   
-binary_bar = np.zeros(500,dtype=int)
+#variable define:
+m,n = dst.shape[:2]
+# print('m=',m,',n=',n)
+bar_y = np.zeros((500,300),dtype=int)
+bar_num = np.zeros((500,300))
 l=0
+length_1=0
+hight_1=0
 for i in range(1,m):
     k = 1
     l = l+1
     for j in range(1,n-1):
+        #compare two color of conjuction points
         if dst[i,j]!=dst[i,j+1]:
             #bar_x(l,k) = i
-            bar_y[l,k]=j
-            k = k+1
+            bar_y[l-1,k-1]=j    #record the changing point /////minus 1
+            k = k+1             #moving to the next point
         if k>61:
             l = l-1
             break
     if k<61:
         l = l-1
 
-print bar_y
+    #save the biggest matrix range
+    if length_1<l-1:
+        length_1=l-1;
+    if hight_1<k-1:
+        hight_1=k-1;
 
 
-m, n = bar_y.shape[:2]
+# print length_1,hight_1
+# print bar_y
+#cutting the oversized simpling matrix
+bar_yy = np.zeros((length_1,hight_1),dtype=int) #define the temp matrix
+for i in range(0,length_1):
+    for j in range(0,hight_1):
+        bar_yy[i,j]=bar_y[i,j]
+# np.delect(bar_y,[0],None)
+bar_y=bar_yy
+# print bar_y
+
+# print(length_1,hight_1)
+m,n = bar_y.shape[:2]
+# print('m=',m,',n=',n)
 
 if m <= 1:
     code = '0'
     print(1,'GameOver~\n')
     
-
-for i in range(1,m):           
-    for j in range(1,n-1):
+#the length of each bar
+for i in range(0,m):           
+    for j in range(0,n-1):
         bar_num[i,j] = bar_y[i,j+1] - bar_y[i,j]
         if bar_num[i,j]<0:
             bar_num[i,j] = 0
-        
-    
+#cutting bar_num
+bar_num_temp = np.zeros((m,n-1)) #define the temp matrix
+for i in range(0,m):
+    for j in range(0,n-1):
+        bar_num_temp[i,j]=bar_num[i,j]
+bar_num=bar_num_temp
 
-bar_sum = sum(bar_num)/m   
+# print bar_num.shape[:2]
+# print bar_num[223,58]
+#average length of each bar
+sum_bar_num=np.zeros(n-1)
+for i in range(0,m-1):
+    for j in range(0,n-1):
+        sum_bar_num[j]=sum_bar_num[j]+bar_num[i,j]
+bar_sum = sum_bar_num/m    
+# print (bar_sum)
+
 k = 0
-for i in range(1,59):   
+for i in range(0,59):   #total length of bar  
     k = k + bar_sum[i]
 
-k = k/95   
-for i in range(1,59): 
+bar_int = np.zeros(n-1,dtype=int)  
+k = k/97    #the length of unit bar
+for i in range(0,59): 
     bar_int[i] = round(bar_sum[i]/k)
+# print bar_int
 
-k = 1
-for i in range(1,59):  
-    if i%2 == 1:
-        for j in range(1,bar_int[i]):  
-            binary_bar[k] = 1
+#change to binary
+binary_bar = np.zeros(95,dtype=int)
+k = 0
+for i in range(0,59):  
+    if i%2 == 0:
+        for j in range(0,bar_int[i]):  
+            binary_bar[k] = 1   #dark is 1
             k = k+1
         
     else:
-        for j in range(1,bar_int[i]):  
-            binary_bar[k] = 0
+        for j in range(0,bar_int[i]):  
+            binary_bar[k] = 0   #white is 0
             k = k+1
-print (binary_bar)       
+# print binary_bar
     
 #########################
 #start to change the binary codes in to bar codes
@@ -135,22 +171,23 @@ print (binary_bar)
 check_left = np.int0([[13,25,19,61,35,49,47,59,55,11],[39,51,27,33,29,57, 5,17, 9,23]])
 check_right = np.int0([114,102,108,66,92,78,80,68,72,116])
 first_num = np.int0([31,20,18,17,12,6,3,10,9,5])
-bar_left = np.zeros(7,dtype=int)
-bar_right = np.zeros(7,dtype=int)
-if ((binary_bar[1] and ~binary_bar[2] and binary_bar[3]) and (~binary_bar[46] and binary_bar[47] and ~binary_bar[48] and binary_bar[49] and ~binary_bar[50]) and (binary_bar[95] and ~binary_bar[94] and binary_bar[93])):
-    l = 1
+bar_left = np.zeros(6,dtype=int)
+bar_right = np.zeros(6,dtype=int)
+if ((binary_bar[0] and ~binary_bar[1] and binary_bar[2]) and (~binary_bar[45] and binary_bar[46] and ~binary_bar[47] and binary_bar[48] and ~binary_bar[49]) and (binary_bar[94] and ~binary_bar[93] and binary_bar[92])):
+    l = 0
     #change the left binary numbers into decimal numbers
-    for i in range(1,6):
+    for i in range(1,7):
         bar_left[l] = 0
-        for k in range(1,7):
-            bar_left[l] = bar_left[l]+binary_bar[7*(i-1)+k+3]*(2^(7-k))
+        for k in range(1,8):
+            bar_left[l] = bar_left[l]+(binary_bar[7*(i-1)+k+2])*(2**(7-k))
         l = l+1
-    l = 1
+    
+    l = 0
     #change the right binary numbers into decimal numbers
-    for i in range(1,6):
+    for i in range(1,7):
         bar_right[l] = 0
-        for k in range(1,7):
-            bar_right[l] = bar_right[l]+binary_bar[7*(i+6)+k+1]*(2^(7-k))
+        for k in range(1,8):
+            bar_right[l] = bar_right[l]+binary_bar[7*(i+6)+k]*(2**(7-k))
             k = k-1
         l = l+1
 
@@ -158,33 +195,38 @@ num_bar = ''
 num_first = 0
 first = 2
 #check the bar codes from the left bar dictionary
-for i in range(1,6):
-    for j in range(0,1):
-        for k in range(0,9):
-            if bar_left[i]==check_left[j+1,k+1]:
-                num_bar = strcat(num_bar , num2str(k));
-                
+for i in range(1,7):
+    for j in range(0,2):
+        for k in range(0,10):
+            if bar_left[i-1]==check_left[j,k]:
+                # num_bar = strcat(num_bar , num2str(k));
+                num_bar += str(k)
+                # print num_bar
                 if first == 0:
-                    num_first = num_first + ~j*(2^(6-i))
+                    if j==0:
+                        num_first = num_first + (2**(6-i))
                 elif first == 1:
-                    num_first = num_first + j*(2^(6-i))
+                    num_first = num_first + j*(2**(6-i))
+                    print num_first
                 elif first == 2:
                     first = j
 
 
 #check the bar codes from the right bar dictionary
-for i in range(1,6):
-    for j in range(0,9):
-        if bar_right[i]==check_right[j+1]:
-            num_bar = strcat(num_bar , num2str(j))
+for i in range(1,7):
+    for j in range(0,10):
+        if bar_right[i-1]==check_right[j]:
+            num_bar += str(j)
+            # num_bar = strcat(num_bar , num2str(j))
 
 #check first bar code from the first bar code dictionary
-for i in range(0,9):
-    if num_first==first_num[i+1]:
-        num_bar = strcat(num2str(i) , num_bar)
+for i in range(0,10):
+    if num_first==first_num[i]:
+        num_bar = str(i)+num_bar
+        # num_bar = strcat(num2str(i) , num_bar)
         break
 
-print ('the bar code is: ',num_bar)
+print 'the bar code is: ',num_bar
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
